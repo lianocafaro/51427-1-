0c2812966c4e6eaaea51cb753d288e181e882d47
lianocafaro <liano.cafaro@gmail.com>
2025-05-19 19:25:16 -0300

    Delete analizador.51427 directory

--- analizador.51427/nodes_module/ErrorListener.js
+++
@@ -1,9 +0,0 @@
-const antlr4 = require('antlr4');
-
-class MiniJSErrorListener extends antlr4.error.ErrorListener {
-    syntaxError(recognizer, offendingSymbol, line, column, msg, e) {
-        console.error(`Error en línea ${line}:${column} - ${msg}`);
-    }
-}
-
-module.exports = MiniJSErrorListener; 
